f2388aca0100dad7b2ce41d116e077003d56a29e
Orlando Quero <orlaqp@gmail.com>
2016-09-14 23:42:05 -0400

    added rounded property to buttons

--- src/user-interface/button/button.component.ts
+++ src/user-interface/button/button.component.ts
@@ -12,6 +12,7 @@ export class ButtonComponent {
     @Input() icon: string;
     @Input() block: boolean;
     @Input() circular: boolean;
+    @Input() rounded: boolean;
 
     @Input() size: string = ''; // lg, sm, xs
     @Input() disabled: boolean;
