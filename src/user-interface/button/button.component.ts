6ea65f9ea6a4de5bc0754714b15a8801d4a7c99a
Orlando Quero <orlaqp@gmail.com>
2016-09-17 08:46:37 -0400

    updated demo and button components to support simple button colors

--- src/user-interface/button/button.component.ts
+++ src/user-interface/button/button.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, OnChanges } from '@angular/core';
 
 @Component({
     moduleId: module.id,
     selector: 'bw-button',
     templateUrl: 'button.component.pug',
 })
-export class ButtonComponent implements OnChanges {
+export class ButtonComponent implements OnInit, OnChanges {
 
     @Input() title: string;
     @Input() color: string;
@@ -14,12 +14,13 @@ export class ButtonComponent implements OnChanges {
     @Input() circular: boolean;
     @Input() rounded: boolean;
     @Input() simple: boolean;
-    @Input() simpleColor: string;
 
     @Input() size: string = ''; // lg, sm, xs
     @Input() disabled: boolean;
 
     @Output() clicked = new EventEmitter();
+
+    simpleColor: string;
 
     constructor() { }
 
@@ -27,12 +28,19 @@ export class ButtonComponent implements OnChanges {
         return this.title !== undefined && this.icon !== undefined && !this.circular;
     }
 
+    ngOnInit() {
+        if (this.simple) {
+            this.simpleColor = this.color;
+            this.color = null;
+        }
+    }
+
     ngOnChanges(changes: {[propertyName: string]: any}) {
         let simple: boolean = changes['simple'];
 
-        if (simple) {
-            this.color = 'white';
-        }
+        // if (simple) {
+        //     this.color = 'white';
+        // }
     }
 
     onClick(e: MouseEvent): void {
