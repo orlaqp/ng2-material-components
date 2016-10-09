c0d3eeb215b882ec654b81e2cbae73e9f379de7a
Julio Vazquez <juliv85@gmail.com>
2016-10-09 11:20:49 -0400

    retrive selected value when select pickker change

--- src/forms/select-picker/select-picker.component.ts
+++ src/forms/select-picker/select-picker.component.ts
@@ -116,6 +116,7 @@ export class SelectPickerComponent extends InputBase implements OnChanges {
                         id: item.id,
                         title: item.title,
                         disabled: item.disabled,
+                        selected: item.selected
                     };
                 });
             }
